0547617e2154e24493be3a122939eff0ed26f347
aditya760 <34020925+aditya760@users.noreply.github.com>
2017-12-21 12:46:14 +0530

    Update unit-test.sh

--- src/cognizant.com/codeblue/ci/unit-test.sh
+++ src/cognizant.com/codeblue/ci/unit-test.sh
@@ -1,1 +1,18 @@
 
+ - -exc
+  - |
+    ls -ltr
+    apt-get update
+    curl https://glide.sh/get | sh
+    ls -ltr
+    go env
+    pwd
+    export GOPATH=$PWD/hello-go
+    cd hello-go/src/cognizant.com/codeblue
+    glide up
+    pwd
+    cd ..
+    cd ..
+    echo "START TESTING"
+    go test cognizant.com/codeblue/test/
+    echo "TEST DONE"
