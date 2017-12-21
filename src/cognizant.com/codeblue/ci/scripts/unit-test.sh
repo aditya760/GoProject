b474351cbfa49873edc81441f2e3858a5c5d509d
aditya760 <aditya.singhal@cognizant.com>
2017-12-21 16:22:29 +0530

    commit scripts

---
+++ src/cognizant.com/codeblue/ci/scripts/unit-test.sh
@@ -0,0 +1,17 @@
+ set -e -x
+    
+    ls -ltr
+    apt-get update
+    curl https://glide.sh/get | sh
+    ls -ltr
+    go env
+    pwd
+    export GOPATH=$PWD/autopcftes
+    cd autopcftes/src/cognizant.com/codeblue
+    glide up
+    pwd
+    cd ..
+    cd ..
+    echo "START TESTING"
+    go test cognizant.com/codeblue/test/
+    echo "TEST DONE"
